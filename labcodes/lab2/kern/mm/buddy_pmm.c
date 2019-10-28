3c9841607f1d9caab7fddf8a86587c577e25b766
yanbin <yanb25@mail2.sysu.edu.cn>
2019-10-28 16:05:16 +0800

    feat enable logging.
    add file libs/logging.h and kern/libs/logging.c.
    
    set a global logging level and a component's logging level to control
    the output of log.
    Every component's log can be controlled seperately in logging.h

--- labcodes/lab2/kern/mm/buddy_pmm.c
+++ labcodes/lab2/kern/mm/buddy_pmm.c
@@ -3,6 +3,7 @@
 #include <buddy_pmm.h>
 #include <x86.h>
 #include <stdio.h>
+#include <logging.h>
 
 static struct buddy_pmm_meta pmm_meta;
 
@@ -11,21 +12,26 @@ static struct buddy_pmm_meta pmm_meta;
 #define cont (pmm_meta.cont_log2)
 #define alloctree_init (pmm_meta.alloctree_init)
 
+/* define logging macros */
+#define mmu_debugf(fmt, ...) debugf(MMU, fmt, ##__VA_ARGS__)
+#define mmu_warnf(fmt, ...) warnf(MMU, fmt, ##__VA_ARGS__)
+#define mmu_infof(fmt, ...) infof(MMU, fmt, ##__VA_ARGS__)
+#define mmu_errorf(fmt, ...) errorf(MMU, fmt, ##__VA_ARGS__)
+#define mmu_panicf(fmt, ...) panicf(MMU, fmt, ##__VA_ARGS__)
 
 static inline void
 report(struct Page* page, size_t n, char* str) {
-    cprintf("[MMU] %s\n", str);
     if (page == NULL) {
-        cprintf("Page Fault: NULL %u page(s)\n\n", n);
+        mmu_infof("%s\nPage Fault: NULL %u page(s)\n", str, n);
         return;
     }
     struct Page *beg = page, *end = page + n - 1;
     uint32_t nbeg = page2ppn(beg), nend = page2ppn(end);
     uintptr_t beg_addr = page2pa(beg), end_addr = page2pa(end);
     if (n == 1) {
-        cprintf("%u page(%u) @0x%08x\n\n", n, nbeg, beg_addr);
+        mmu_infof("%s\n%u page(%u) @0x%08x\n", str, n, nbeg, beg_addr);
     } else {
-        cprintf("%u pages(%u - %u) @0x%08x - @0x%08x\n\n", n, nbeg, nend, beg_addr, end_addr);
+        mmu_infof("%s\n%u pages(%u - %u) @0x%08x - @0x%08x\n", str, n, nbeg, nend, beg_addr, end_addr);
     }
 }
 
@@ -71,19 +77,22 @@ buddy_init_alloctree(void) {
     alloctree_init = 1;
 
     buddy_rollup_nr_free = ROLLUP_POW2(buddy_nr_free);
-    cprintf("[MMU] nr_free = %u, roll up nr_free = %u\n", buddy_nr_free, buddy_rollup_nr_free);
+    mmu_debugf("nr_free = %u, roll up nr_free = %u\n", buddy_nr_free, buddy_rollup_nr_free);
     do_init_alloctree(1, buddy_rollup_nr_free, buddy_nr_free);
-    cprintf("[MMU] showing cont info ...\n");
+
+    RAW_LOGGING
+    mmu_infof("showing cont info ...\n");
     for (int i = 1; i < 20; ++i) {
-        cprintf("\t %d:%u ", i, cont[i]);
+        mmu_infof("%d:%u ", i, cont[i]);
     }
-    cprintf("\n");
+    mmu_infof("\n");
+    ENDR
 }
 
 static struct Page*
 do_alloc_pages(size_t idx, size_t n) {
     assert(idx > 0 && idx < BUDDY_NPG);
-    cprintf("[debug] calling alloc pages, idx=%u, cont=%u\n", idx, cont[idx]);
+    RAW_LOGGING mmu_debugf("calling alloc pages, idx=%u, cont=%u\n", idx, cont[idx]); ENDR
 
     /* quick match */
     if (idx2nodesize(idx) == n && !USED(idx)) {
@@ -118,6 +127,7 @@ buddy_alloc_pages(size_t n) {
         buddy_init_alloctree();
     }
     uint32_t rollup_n = ROLLUP_POW2(n);
+    mmu_debugf("start to do alloc page(s)\n");
     struct Page* p = do_alloc_pages(HEAD, rollup_n);
     report(p, rollup_n, "buddy_alloc_pages");
     return p;
@@ -133,7 +143,7 @@ do_free_pages_backwords(size_t idx) {
             cont[idx] = max(cont[LEFT(idx)], cont[RIGHT(idx)]);
         }
     }
-    cprintf("\t backwords with idx=%u, cont=%u\n", idx, cont[idx]);
+    RAW_LOGGING mmu_debugf("backwords with idx=%u, cont=%u\n", idx, cont[idx]); ENDR
     if (!first_layer(idx)) {
         do_free_pages_backwords(PAR(idx));
     }
@@ -154,7 +164,7 @@ buddy_free_pages(struct Page* base, size_t n) {
 
     uint32_t rollup_n = ROLLUP_POW2(n);
 
-    cprintf("\t nodesize=%u, depth=%u, debug=%u, fst=%u, buddy_rullup_nr_free=%u\n", rollup_n, nodesize2depth(rollup_n), buddy_rollup_nr_free/rollup_n, nodesize2fstidx(rollup_n), buddy_rollup_nr_free);
+    mmu_debugf("nodesize=%u, depth=%u, debug=%u, fst=%u, buddy_rullup_nr_free=%u\n", rollup_n, nodesize2depth(rollup_n), buddy_rollup_nr_free/rollup_n, nodesize2fstidx(rollup_n), buddy_rollup_nr_free);
 
     bool found = 0;
     size_t cur_idx = HEAD;
@@ -162,17 +172,18 @@ buddy_free_pages(struct Page* base, size_t n) {
     while (!out_of_range(cur_idx)) {
         uint32_t nodesize = idx2nodesize(cur_idx);
         ppn_t ppn = idx2ppn(cur_idx);
-        cprintf("\t searching idx=%u, n=%u, bppn=%u, nodesize=%u, ppn=%u\n", cur_idx, n, bppn, nodesize, ppn);
+        RAW_LOGGING 
+        mmu_debugf("searching idx=%u, n=%u, bppn=%u, nodesize=%u, ppn=%u\n", cur_idx, n, bppn, nodesize, ppn); ENDR
         if (nodesize == n && ppn == bppn) {
             /* parameter match */
             if (cont[cur_idx] == 0) {
-                cprintf("\t freeing %u, backward...\n", cur_idx);
+                mmu_debugf("freeing %u, backward...\n", cur_idx);
                 cont[cur_idx] = n;
                 do_free_pages_backwords(cur_idx);
                 found = 1;
                 break;
             } else {
-                cprintf("\t WARN: cont[cur_idx]=%u not ZERO\n", cont[cur_idx]);
+                mmu_warnf("WARN: cont[cur_idx]=%u not ZERO\n", cont[cur_idx]);
             }
         } else if (idx2ppn(RIGHT(cur_idx)) > bppn) {
             assert(idx2ppn(LEFT(cur_idx)) <= bppn);
@@ -183,16 +194,7 @@ buddy_free_pages(struct Page* base, size_t n) {
         }
     }
 
-    // for (size_t idx = beg_idx; idx < end_idx; ++idx) {
-    //     cprintf("cont[idx] = %u, idx2lgppn=%u, page2lgppn=%u\n", cont[idx], idx2lgppn(idx), page2lgppn(base));
-    //     if (cont[idx] == 0 && idx2lgppn(idx) == page2lgppn(base)) {
-    //         cont[idx] = n;
-    //         do_free_pages_backwords(idx);
-    //         found = 1;
-    //         break;
-    //     }
-    // }
-    cprintf("end\n");
+    RAW_LOGGING mmu_debugf("end\n"); ENDR
     if (!found) {
         report(base, rollup_n, "WARN, fails to free pages.");
     } else {
@@ -215,7 +217,7 @@ one_page_check(void) {
     assert((p0 = alloc_page()) != NULL);
     assert((p1 = alloc_page()) != NULL);
     assert((p2 = alloc_page()) != NULL);
-    cprintf("now freeing ...\n");
+    mmu_debugf("now freeing ...\n");
 
     free_page(p1);
     free_page(p0);
@@ -230,14 +232,14 @@ one_page_check(void) {
         free_page(page[i]);
     }
 
-    cprintf("+++++++ MMU reset ++++++++\n");
+    mmu_debugf("+++++++ MMU reset ++++++++\n");
     alloctree_init = 0;
     buddy_nr_free = nr_free_store;
 }
 
 static void
 pages_check() {
-    cprintf("++++++++ starting pages check +++++++++\n");
+    mmu_debugf("++++++++ starting pages check +++++++++\n");
     uint32_t nr_free_store = buddy_nr_free;
     buddy_nr_free = 16;
 
@@ -260,14 +262,14 @@ pages_check() {
     for (int i = 0; i < 8; ++i) {
         free_pages(page[i], 2);
     }
-    cprintf("--------- MMU reset --------\n");
+    mmu_debugf("--------- MMU reset --------\n");
     alloctree_init = 0;
     buddy_nr_free = nr_free_store;
 }
 
 static void
 random_alloc_check(void) {
-    cprintf("+++++++ starting random_alloc_check ++++++++\n");
+    mmu_debugf("+++++++ starting random_alloc_check ++++++++\n");
     uint32_t nr_free_store = buddy_nr_free;
     buddy_nr_free = 16;
 
@@ -285,7 +287,7 @@ random_alloc_check(void) {
     for (int i = 0; i < 4; ++i) {
         free_page(page[i]);
     }
-    cprintf("--------- MMU reset --------\n");
+    mmu_debugf("--------- MMU reset --------\n");
     alloctree_init = 0;
     buddy_nr_free = nr_free_store;
 }
@@ -294,7 +296,7 @@ static void
 rollup_alloc_check(void) {
     uint32_t nr_free_store = buddy_nr_free;
     buddy_nr_free = 9;
-    cprintf("+++++++ starting rollup_alloc_check ++++++++\n");
+    mmu_debugf("+++++++ starting rollup_alloc_check ++++++++\n");
 
     struct Page *p0, *p1, *p2;
     assert((p0 = alloc_pages(8)) != NULL);
@@ -321,7 +323,7 @@ rollup_alloc_check(void) {
         free_page(page[i]);
     }
 
-    cprintf("--------- MMU reset --------\n");
+    mmu_debugf("--------- MMU reset --------\n");
     alloctree_init = 0;
     buddy_nr_free = nr_free_store;
 }
