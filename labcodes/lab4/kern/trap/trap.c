de1da658f53b36932d977ceface297cda8905fa7
yanbin <yanb25@mail2.sysu.edu.cn>
2019-11-02 12:54:00 +0800

    bug still here.

--- labcodes/lab4/kern/trap/trap.c
+++ labcodes/lab4/kern/trap/trap.c
@@ -18,14 +18,6 @@
     infof(KERNEL, fmt, ##__VA_ARGS__)
 
 #define TICK_NUM 100
-// number of interrupt that is reserved by intel
-#define N_RESERVED_INT 32
-#define N_INT 256
-/* selectors */
-#define PROT_MODE_CSEG 0x8
-#define PROT_MODE_DSEG 0x10
-#define PROT_MODE_UCSEG 0x1B
-#define PROT_MODE_UDSEG 0x23
 
 static void print_ticks() {
     cprintf("%d ticks\n",TICK_NUM);
@@ -35,48 +27,13 @@ static void print_ticks() {
 #endif
 }
 
-static int do_switch_to_user(struct trapframe *tf) {
-    if (tf->tf_cs != USER_CS) {
-        tf->tf_ds = PROT_MODE_UDSEG;
-        tf->tf_es = PROT_MODE_UDSEG;
-        tf->tf_fs = PROT_MODE_UDSEG;
-        tf->tf_gs = PROT_MODE_UDSEG;
-        tf->tf_ss = PROT_MODE_UDSEG;
-        tf->tf_cs = PROT_MODE_UCSEG; /* Code Selector Here */
-        tf->tf_eflags &= ~FL_IOPL_MASK;
-        tf->tf_eflags |= FL_IOPL_3; /* set IOPL=3 to allow ring3 to access IO port */
-        return 0;
-    }
-    return 1;
-}
-
-static int do_switch_to_kernel(struct trapframe *tf) {
-    if (tf->tf_cs != KERNEL_CS) {
-        cprintf("Switching to kernel...\n");
-        tf->tf_ds = PROT_MODE_DSEG;
-        tf->tf_es = PROT_MODE_DSEG;
-        tf->tf_fs = PROT_MODE_DSEG;
-        tf->tf_gs = PROT_MODE_DSEG;
-        tf->tf_ss = PROT_MODE_DSEG;
-        tf->tf_cs = PROT_MODE_CSEG; /* Code Selector Here */
-        /* clear bits */
-        tf->tf_eflags &= ~FL_IOPL_MASK;
-        /* reset to IOPL=0 */
-        tf->tf_eflags |= FL_IOPL_0;
-        return 0;
-    }
-    return 1;
-}
-
-
-
 /* *
  * Interrupt descriptor table:
  *
  * Must be built at run time because shifted function addresses can't
  * be represented in relocation records.
  * */
-static struct gatedesc idt[N_INT] = {{0}};
+static struct gatedesc idt[256] = {{0}};
 
 static struct pseudodesc idt_pd = {
     sizeof(idt) - 1, (uintptr_t)idt
@@ -100,15 +57,13 @@ idt_init(void) {
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
 
-    for (int i = 0; i < N_INT; ++i) {
-        SETGATE(idt[i], 0, PROT_MODE_CSEG, __vectors[i], DPL_KERNEL);
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
     }
-    // set DPL to DPL_USER to permit user to use `int 80`
-    SETGATE(idt[T_SYSCALL], 1, PROT_MODE_CSEG, __vectors[T_SYSCALL] , DPL_USER);
-    /* temporary open this int */
-    SETGATE(idt[T_SWITCH_TOK], 1, PROT_MODE_CSEG, __vectors[T_SWITCH_TOK] , DPL_USER);
-    // load IDT
     lidt(&idt_pd);
+    
 }
 
 static const char *
@@ -256,31 +211,12 @@ trap_dispatch(struct trapframe *tf) {
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
         cprintf("serial [%03d] %c\n", c, c);
-        if (c == '0') {
-            if (!do_switch_to_kernel(tf)) {
-                print_trapframe(tf);
-            }
-        } else if (c == '3') {
-            if (!do_switch_to_user(tf)) {
-                print_trapframe(tf);
-            }
-        }
         break;
     case IRQ_OFFSET + IRQ_KBD:
         c = cons_getc();
         cprintf("kbd [%03d] %c\n", c, c);
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
-    case T_SWITCH_TOU:
-        if (do_switch_to_user(tf)) {
-            panic("unexpected T_SWITCH_TOU in user space detected.\n");
-        }
-        break;
-    case T_SWITCH_TOK:
-        if (do_switch_to_kernel(tf)) {
-            panic("unexpected T_SWITCH_TOK in user space detected.\n");
-        }
-        break;
     case T_SYSCALL:
         /* empty statment here to workaround */ ;
         uint32_t eax = tf->tf_regs.reg_eax;
